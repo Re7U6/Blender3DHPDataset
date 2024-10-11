d7e98da3b8e675c63b0373528c8b230141c6a8b6
Re7U6 <masuryui0829@gmail.com>
2024-10-12 00:30:13 +0900

    update:npzの出力に対応

--- genDataset3D.py
+++ genDataset3D.py
@@ -1,16 +1,12 @@
-from distutils.core import setup
-
 import bpy
-import bmesh
 import math
-import os
-import random
-import json
-
 from pathlib import Path
+from tqdm import tqdm
 
-from blenderproc.python.types.MeshObjectUtility import scene_ray_cast
-from mathutils import Vector
+import bmesh
+import numpy as np
+# np.set_printoptions(suppress=True)
+
 
 # H3.6Mに準ずる32個の関節点のうち、動作に関わる17個の関節点を用いる
 # H3.6Mとbvhのキーポイントの対応付けを以下のように定義する
@@ -33,6 +29,7 @@ bone_mapping = {
     'UpperArm_L': 'LElbow',
     'LowerArm_L': 'LWrist'
 }
+
 bone_order = {
     'Hip': 0,
     'RHip': 1,
@@ -53,26 +50,35 @@ bone_order = {
     'RWrist': 16
 }
 
-
+subjects=['Train', 'Validate']
 
 # bandaiデータセットを使う場合、rootと手は使わないので隠す
 # ただし足はヘッドを使うため残す
 bones_to_hide = ['joint_Root', 'Hand_R', 'Hand_L', 'Toes_R', 'Toes_L']
 
 
+def initialize_armature():
+    '''
+    アーマチュアの初期化
+    '''
+    # アーマチュアとアクションデータを削除
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.ops.object.select_all(action='DESELECT')
+    for obj in bpy.data.objects:
+        if obj.type == 'ARMATURE':
+            obj.select_set(True)
+        else:
+            obj.select_set(False)
+    bpy.ops.object.delete()
+    bpy.ops.outliner.orphans_purge()
+    # for action in bpy.data.actions:
+    #     bpy.data.actions.remove(action)
+
+
 def setup_bvh(bvh_file):
     '''
     モーションデータ（.bvh）のセットアップ
     '''
-
-    # すべてのオブジェクト、モーションデータを削除
-    bpy.ops.object.mode_set(mode='OBJECT')
-    bpy.ops.object.select_all(action='SELECT')
-    bpy.ops.object.delete()
-    bpy.ops.outliner.orphans_purge()
-    for action in bpy.data.actions:
-        bpy.data.actions.remove(action)
-
     # import bvh
     bpy.ops.import_anim.bvh(
         filepath=str(bvh_file),
@@ -87,7 +93,9 @@ def setup_bvh(bvh_file):
         axis_up='Y'
     )
 
-    armature = bpy.data.objects[-1]
+    for obj in bpy.data.objects:
+        if obj.type == 'ARMATURE':
+            armature = obj
     bpy.ops.object.mode_set(mode='EDIT')
 
     for bone_name in bones_to_hide:
@@ -95,7 +103,26 @@ def setup_bvh(bvh_file):
         if bone:
             armature.data.edit_bones.remove(bone)
 
+    # Headが無いときは
+    # for bone_name in bones_to_hide:
+    #     bone = armature.data.edit_bones.get(bone_name)
+    #     if bone:
+    #         armature.data.edit_bones.remove(bone)
+    #
+    # edit_bones = armature.data.edit_bones
+    # head_bone = edit_bones.get('Head')
+    # neck_bone = edit_bones.get('Neck')
+    # if head_bone.length < 0.5:
+    #     edit_bones.remove(head_bone)
+    #     new_head_bone = edit_bones.new('Head')
+    #     direction = (neck_bone.tail - neck_bone.head).normalized()
+    #     head_length = neck_bone.length
+    #     new_head_bone.head = neck_bone.tail
+    #     new_head_bone.tail = neck_bone.tail + direction * head_length
+    #     new_head_bone.parent = neck_bone
+
     bpy.ops.object.mode_set(mode='OBJECT')
+
 
 def get_keyframe_range():
     '''
@@ -108,6 +135,7 @@ def get_keyframe_range():
                 for keyframe in fcurve.keyframe_points:
                     keyframes.add(int(keyframe.co[0]))
     return min(keyframes), max(keyframes)
+
 
 def setup_environment(radius=5, segments=8, ring_count=10, focal_length=35.0):
     '''
@@ -127,9 +155,10 @@ def setup_environment(radius=5, segments=8, ring_count=10, focal_length=35.0):
 
     # 球全体を下に動かし、球の下半分と一番上の頂点を削除
     for vert in mesh.verts:
-        vert.co.z -= 0.5
+        vert.co.z *= 0.4
+        # vert.co.z -= 0.5
         vert.select = False
-        if vert.co.z < -0.5 or vert.co.z == 4.5:
+        if vert.co.z < -0.5 or vert.co.z == 2:
             vert.select = True
 
     bmesh.ops.delete(mesh, geom=[v for v in mesh.verts if v.select], context='VERTS')
@@ -137,7 +166,6 @@ def setup_environment(radius=5, segments=8, ring_count=10, focal_length=35.0):
     bpy.ops.object.mode_set(mode='OBJECT')
 
     vertex_count = len(uv_sphere.data.vertices)
-
 
     # カメラを設定
     bpy.ops.object.camera_add()
@@ -152,6 +180,7 @@ def setup_environment(radius=5, segments=8, ring_count=10, focal_length=35.0):
     constraint.up_axis = 'UP_Y'
 
     return sphere_name, camera_name, vertex_count
+
 
 def setup_camera(vertex_index, sphere_name='Sphere', camera_name='Camera'):
     '''
@@ -168,13 +197,14 @@ def setup_camera(vertex_index, sphere_name='Sphere', camera_name='Camera'):
         vertex_world_position = obj.matrix_world @ obj.data.vertices[vertex_index].co
         camera.location = vertex_world_position
 
+    bpy.context.view_layer.update()
     camera_position = camera.location
     camera_world_rotation = camera.matrix_world.to_quaternion()
     quaternion = camera_world_rotation
     azimuth = camera_world_rotation.to_euler()[2]
     azimuth = round(math.degrees(azimuth))
 
-    return camera_position, azimuth, quaternion
+    return [camera_position, azimuth, quaternion]
 
 
 def camera_resolution(x=224, y=224, per=100):
@@ -184,9 +214,6 @@ def camera_resolution(x=224, y=224, per=100):
     bpy.context.scene.render.resolution_x = x
     bpy.context.scene.render.resolution_y = y
     bpy.context.scene.render.resolution_percentage = per
-
-def get_3d_coordinates():
-
 
 
 def get_coordinates(scene, camera, bone, resolution_x, resolution_y):
@@ -227,20 +254,18 @@ def get_coordinates(scene, camera, bone, resolution_x, resolution_y):
 
     return [coordinate_x, coordinate_y], list(bone_world_pos), bone.name
 
+
 def sort_keypoints(coordinats, bone_name, keypoint_list):
+    '''
+    キーポイントの並び替え
+    '''
     key = bone_mapping[bone_name]
     value = bone_order[key]
     keypoint_list[value] = coordinats
 
 
-def save_keypoints(annotations, output_path):
-    with open(os.path.join(output_path, 'pose.json'), 'w') as f:
-        for anno in annotations:
-            line = json.dumps(anno)
-            f.write(line + '\n')
 
-
-def main(num_data, input_path, output_path):
+def main(input_path, output_path):
     resolution_x = 1920 # x横解像度
     resolution_y = 1080 # y縦解像度
 
@@ -250,13 +275,27 @@ def main(num_data, input_path, output_path):
     # カメラ解像度の初期化
     camera_resolution(resolution_x, resolution_y, 100)
 
-    annotations = []
-    # 環境の作成
+    # 環境の初期化
+    if bpy.ops.objects:
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete()
+    bpy.ops.outliner.orphans_purge()
+    for action in bpy.data.actions:
+        bpy.data.actions.remove(action)
+
+    # 環境の設定
     sphere_name, camera_name, vertex_count = setup_environment(radius=5, segments=8, ring_count=10, focal_length=35.0)
 
-    for i in range(num_data):
-        # bvhデータのロード
-        bvh_file = random.choice(bvh_files)
+    output_3d = {}
+    output_2d = {}
+
+    for idx, bvh_file in enumerate(tqdm(bvh_files, desc=f'bvh処理ちう')):
+        output_3d[f'data{idx+1}'] = {}
+        output_2d[f'data{idx+1}'] = {}
+        # アーマチュア周りを初期化
+        initialize_armature()
+
+        # bvhデータをロード
         setup_bvh(bvh_file)
 
         armature = None
@@ -265,16 +304,15 @@ def main(num_data, input_path, output_path):
                 armature = obj
                 break
 
-        keypoints_2d = []
-        keypoints_3d = []
-        camera = []
-        # キーフレーム範囲を取得
-        start_frame, end_frame = get_keyframe_range()
-
-        for vertex_index in range(vertex_count):
+        for vertex_index in tqdm(range(vertex_count), desc=f'bvh処理状況{idx+1}/{len(bvh_files)}'):
             # カメラの位置設定
-            camera_position, azimuth, quaternion, scene = setup_camera(vertex_index, sphere_name, camera_name)
+            camera_status = setup_camera(vertex_index, sphere_name, camera_name)
 
+            # キーフレーム範囲を取得
+            start_frame, end_frame = get_keyframe_range()
+
+            positions_2d = []
+            positions_3d = []
             for frame in range(start_frame, end_frame + 1):
                 # フレームのロード
                 bpy.context.scene.frame_set(frame)
@@ -283,22 +321,46 @@ def main(num_data, input_path, output_path):
 
                 keypoint_2d = [0 for _ in range(17)]
                 keypoint_3d = [0 for _ in range(17)]
+
                 # ボーンのカメラビューにおける2D座標を取得
                 for bone in armature.pose.bones:
                     coordinates2d, coordinates3d,bone_name = get_coordinates(scene, camera, bone, resolution_x, resolution_y)
-                    if 0 in coordinates2d:
-                        break
                     sort_keypoints(coordinates2d, bone_name, keypoint_2d)
                     sort_keypoints(coordinates3d, bone_name, keypoint_3d)
 
-                keypoints_2d.append(keypoint_2d)
-                keypoints_3d.append(keypoint_2d)
+                # 2d座標に0を含まないようにする
+                if all(0 not in i for i in keypoint_2d):
+                    positions_2d.append(keypoint_2d)
+                    positions_3d.append(keypoint_3d)
+
+            # 空データは使わない
+            if len(positions_2d) != 0:
+                positions_2d = np.round(np.array(positions_2d, dtype=np.float32), 6)
+                positions_3d = np.round(np.array(positions_3d, dtype=np.float32), 6)
+                output_2d[f'data{idx+1}'][f'C{vertex_index+1}'] = positions_2d
+                output_3d[f'data{idx+1}'][f'C{vertex_index+1}'] = positions_3d
+
+        if idx == 2:
+            break
+
+        # カメラ情報を保存
+        # for vertex_index in range(vertex_count):
+        #     camera_status = setup_camera(vertex_index, sphere_name, camera_name)
+        #     with open('camera.txt', 'a') as file:
+        #         file.write(f"C{vertex_index+1}: {camera_status}\n")
 
 
+    print("3dデータ保存ちう...")
+    np.savez_compressed('data_3d_blender.npz', positions_3d=output_3d)
 
+    metadata = {
+        'num_joints': 17,
+        'keypoints_symmetry': [[4, 5, 6, 11, 12, 13], [1, 2, 3, 14, 15, 16]]
+    }
+    print("2dデータ保存ちう...")
+    np.savez_compressed('data_2d_blender.npz', positions_2d=output_2d, metadata=metadata)
 
 if __name__ == '__main__':
     input_path = '/home/masuryui/Bandai-Namco-Research-Motiondataset/dataset/Bandai-Namco-Research-Motiondataset-1/data'
     output_path = '/home/masuryui/Desktop/data'
-    num_data = 50
-    main(num_data, input_path, output_path)
+    main(input_path, output_path)
