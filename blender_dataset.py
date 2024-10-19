79993d21d935b3c5f3b5e8b9fffd0c3269fe2204
Re7U6 <masuryui0829@gmail.com>
2024-10-19 17:28:55 +0900

    余計な部分を削除

--- blender_dataset.py
+++ blender_dataset.py
@@ -1,103 +1,48 @@
 import numpy as np
 import copy
+import ast
 from common.skeleton import Skeleton
 from common.mocap_dataset import MocapDataset
 from common.camera import normalize_screen_coordinates
 
-blender_skeleton = Skeleton(parents=[-1, 0, 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 10, 8,
-                            11, 12, 13, 8, 14, 15, 16],
+# スケルトンの親子関係と左右の定義
+blender_skeleton = Skeleton(parents=[-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8,14, 15],
                             joints_left=[4, 5, 6, 11, 12, 13],
                             joints_right=[1, 2, 3, 14, 15, 16])
 
-blender_cameras_intrinsic_params = [
-    {
-        'id': '',
-        'center': [],
-        'focal_length': [],
-        'radial_distortion': [],
-        'tangential_distortion': [],
-        'res_w': 1,
-        'res_h': 1,
-        'azimuth': 1,
-    },
-    {
-        'id': '',
-        'center': [],
-        'focal_length': [],
-        'radial_distortion': [],
-        'tangential_distortion': [],
-        'res_w': 1,
-        'res_h': 1,
-        'azimuth': 1,
-    },
-]
+# カメラの内部・外部パラメータをtxtファイルから読み込み
+with open('common/camera_intrinsics.txt', 'r') as file:
+   intrinsic = file.read()
+blender_cameras_intrinsic_params = ast.literal_eval(intrinsic)
 
-blender_cameras_extrinsic_params = {
-    'S1': [
-        {
-            'orientation': [],
-            'translation': [],
-        },
-        {
-            'orientation': [],
-            'translation': [],
-        },
-        {
-            'orientation': [],
-            'translation': [],
-        },
-        {
-            'orientation': [],
-            'translation': [],
-        },
-    ],
-    'S2': [
-        {
-            'orientation': [],
-            'translation': [],
-        },
-        {
-            'orientation': [],
-            'translation': [],
-        },
-        {
-            'orientation': [],
-            'translation': [],
-        },
-        {
-            'orientation': [],
-            'translation': [],
-        },
-    ],
-}
+with open('common/camera_extrinsics.txt', 'r') as file:
+   extrinsic = file.read()
+blender_cameras_extrinsic_params = ast.literal_eval(extrinsic)
 
-class blenderDataset(MocapDataset):
+
+class BlenderDataset(MocapDataset):
     def __init__(self, path):
-        super().__init__(fps=60, skeleton=blender_skeleton)
+        super().__init__(fps=30, skeleton=blender_skeleton)
 
-        self.cameras = copy.deepcopy(blender_cameras_extrinsic_params)
+        self._cameras = copy.deepcopy(blender_cameras_extrinsic_params)
         for cameras in self._cameras.values():
             for i, cam in enumerate(cameras):
                 cam.update(blender_cameras_intrinsic_params[i])
                 for k, v in cam.items():
                     if k not in ['id', 'res_w', 'res_h']:
-                        cam[k] = np.array(v, dtype=np.float32)
+                        cam[k] = np.array(v, dtype='float32')
 
                 # カメラフレームの正規化
-                if 'center' in cam:
-                    cam['center'] = normalize_screen_coordinates(cam['center'], w=cam['res_w'], h=cam['res_h']).astype(
-                        'float32')
-                if 'focal_length' in cam:
-                    cam['focal_length'] = cam['focal_length'] / cam['res_w'] * 2
+                cam['center'] = normalize_screen_coordinates(cam['center'], w=cam['res_w'], h=cam['res_h']).astype('float32')
+                cam['focal_length'] = cam['focal_length'] / cam['res_w'] * 2
                 if 'translation' in cam:
                     cam['translation'] = cam['translation']/1000 # ミリメートルをメートルに変換
 
                 # 固有パラメータ・ベクトルの追加
-                if 'center' in cam:
-                    cam['intrinsic'] = np.concatenate((cam['focal_length'],
-                                                       cam['center'],
-                                                       cam['radial_distortion'],
-                                                       cam['tangential_distortion']))
+                cam['intrinsic'] = np.concatenate((cam['focal_length'],
+                                                   cam['center'],
+                                                   cam['radial_distortion'],
+                                                   cam['tangential_distortion']))
 
         # シリアライズされたデータセットを読み込む
         data = np.load(path, allow_pickle=True)['positions_3d'].item()
